1626238c8a1966894b15a671cef900e0b6075a19
Leone <leocr_lem@yahoo.com.br>
2022-04-19 19:06:51 -0300

    Adiconando verbo patch para atulizar parcial

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/IPersonRepository.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/IPersonRepository.cs
@@ -1,15 +1,9 @@
 ﻿using RestWithASPNETUdemy.Model;
-using System.Collections.Generic;
 
 namespace RestWithASPNETUdemy.Repository
 {
-    public interface IPersonRepository
+    public interface IPersonRepository : IRepository<Person>
     {
-        Person Create(Person person);
-        Person FindByID(long id);
-        List<Person> FindAll();
-        Person Update(Person person);
-        void Delete(long id);
-        bool Exists(long id);
+        Person Disable(long id);
     }
 }
